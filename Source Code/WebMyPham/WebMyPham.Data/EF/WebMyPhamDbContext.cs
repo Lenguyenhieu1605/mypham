f7e2cc64781c2afd33478a90d229f7dfad1dd36c
giahantran <17110133@student.hcmute.edu.vn>
2020-10-05 23:22:35 +0700

    add project

---
+++ Source Code/WebMyPham/WebMyPham.Data/EF/WebMyPhamDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMyPham.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebMyPham.Data.Configurations;
+
+namespace WebMyPham.Data.EF
+{
+    public class WebMyPhamDbContext : DbContext
+    {
+        public WebMyPhamDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            //base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}

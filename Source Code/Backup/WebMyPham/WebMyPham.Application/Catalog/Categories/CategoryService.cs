965d13c06d66c403e57bb5c1622b7ff294eddbc9
giahantran <17110133@student.hcmute.edu.vn>
2020-12-01 17:10:41 +0700

    Add project backup

---
+++ Source Code/Backup/WebMyPham/WebMyPham.Application/Catalog/Categories/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebMyPham.Data.EF;
+using WebMyPham.ViewModels.Catalog.Categories;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebMyPham.Application.Catalog.Categories
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly WebMyPhamDbContext _context; //chỉ gán 1 lần
+        public CategoryService(WebMyPhamDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<CategoryViewModel>> GetAll()
+        {
+            var query = from c in _context.Categories
+                        select new { c };
+            return await query.Select(x => new CategoryViewModel()
+            {
+                Id = x.c.Id
+
+            }).ToListAsync();
+        }
+    }
+}
---
+++ Source Code/Backup/WebMyPham/WebMyPham.ViewModels/Common/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebMyPham.ViewModels.Common
+{
+    public class PagedResult<T> : PagedResultBase
+    {
+        public List<T> Items { set; get; } //generic // có thể dùng cho tất cả đối tượng khác nhau
+    }
+}

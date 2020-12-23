124d8b09077680ad999d08164a07f743e5acc98f
DIEMVO <43290038+DIEMVO@users.noreply.github.com>
2020-12-23 19:53:13 +0700

    Cập nhật chính thức
    Cập nhật bản đầy đủ

---
+++ Bang_chinh_thuc/WebMyPham_test/WebMyPham_test/WebMyPham.Data/Entities/CategoryTranslation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebMyPham.Data.Entities
+{
+    public class CategoryTranslation
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+
+    }
+}

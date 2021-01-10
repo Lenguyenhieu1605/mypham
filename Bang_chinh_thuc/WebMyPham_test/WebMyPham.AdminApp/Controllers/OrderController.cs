a90ff3eaa636046567ee05db618d2b81a252b476
DIEMVO <43290038+DIEMVO@users.noreply.github.com>
2021-01-11 01:50:59 +0700

    update code UI + data
    Cập nhật UI + data + role + ppt

--- Bang_chinh_thuc/WebMyPham_test/WebMyPham.AdminApp/Controllers/OrderController.cs
+++ Bang_chinh_thuc/WebMyPham_test/WebMyPham.AdminApp/Controllers/OrderController.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace WebMyPham.AdminApp.Controllers
 {
-    [Authorize(Roles ="admin,shipper")]
+    [Authorize(Roles ="admin, shipper")]
     public class OrderController : Controller
     {
         private readonly IOrderApiClient _orderApiClient;
